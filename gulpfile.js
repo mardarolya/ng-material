76fa50c0cc31543b7b45a6f680b192c370c0d46a
Мардарь Ольга (Mardar Olga) <omardar@beua.com>
2017-01-30 11:07:59 +0200

    flex and s-desctop

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,88 @@
-'use strict';
+"use strict";
 
 var gulp = require("gulp"),
-	jade = require('gulp-jade'),
-	less = require('gulp-less'),
-	tscr = require('gulp-typescript'),
-	cssmin = require('gulp-cssmin'),
-	jsmin  = require('gulp-jsmin'),
-	watch  = require('gulp-watch'),
-	livereload = require('gulp-livereload');
+	jade = require("gulp-jade"),
+	less = require("gulp-less"),
+	tscr = require("gulp-typescript"),
+	cssmin = require("gulp-cssmin"),
+	jsmin  = require("gulp-jsmin"),
+	watch  = require("gulp-watch"),
+	browserSync = require("browser-sync");
+
+gulp.task("browser-sync", function() { // Создаем таск browser-sync
+    browserSync({ // Выполняем browser Sync
+        server: { // Определяем параметры сервера
+            baseDir: "./" // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
 
 // следим за изменениями
-gulp.task('whats-new', function(){
-	livereload.listen(); 
+gulp.task("whats-new", ["browser-sync"], function(){
 	
-	watch(['./src/index.jade', './src/view/**/*.jade'])  
+	watch(["./src/index.jade", "./src/view/**/*.jade"])  
 		.on("change", function(file) {
             gulp.src(file)
             .pipe(jade({pretty: true}))
-            .on('error', console.log)
+            .on("error", console.log)
             .pipe(gulp.dest(function(file){
                     return file.base;
                 }, {overwrite: true}))
-            .pipe(livereload());
+            .pipe(browserSync.reload({stream: true}));
         });
 
-	watch(['./src/script/*.ts', './src/view/**/*.ts'])
+	watch(["./src/script/*.ts", "./src/view/**/*.ts"])
     	.on("change", function(file) {
 	            gulp.src(file)
 	            .pipe(tscr())
-	            .on('error', console.log)
+	            .on("error", console.log)
 	            .pipe(gulp.dest(function(file){
                     return file.base;
                 }, {overwrite: true}))
-                .pipe(livereload());
+                .pipe(browserSync.reload({stream: true}));
 	        });
 	
-	watch('./src/style/*.less')
+	watch("./src/style/*.less")
 		.on("change", function(file) {
             gulp.src(file)
             .pipe(less({pretty: true}))
-            .on('error', console.log)
+            .on("error", console.log)
             .pipe(gulp.dest(function(file){
                     return file.base;
                 }, {overwrite: true}))
-            .pipe(livereload());
+            .pipe(browserSync.reload({stream: true}));
         });
 });	
 
 
 // собираем в public
-gulp.task('dist', function(){
+gulp.task("dist", function(){
 	// перенесем шрифты
-	gulp.src('./src/font/icons/*')
-	    .pipe(gulp.dest('./dist/font/icons'), {overwrite: true}); 
-	gulp.src('./src/font/text/*')
-	    .pipe(gulp.dest('./dist/font/text'), {overwrite: true});     
+	gulp.src("./src/font/icons/*")
+	    .pipe(gulp.dest("./dist/font/icons"), {overwrite: true}); 
+	gulp.src("./src/font/text/*")
+	    .pipe(gulp.dest("./dist/font/text"), {overwrite: true});     
 	// перенесем стили
-	gulp.src('./src/style/*.css')
+	gulp.src("./src/style/*.css")
 		.pipe(cssmin())	
-		.on('error', console.log)
-	    .pipe(gulp.dest('./dist/style'), {overwrite: true}); 
+		.on("error", console.log)
+	    .pipe(gulp.dest("./dist/style"), {overwrite: true}); 
 	// перенесем скрипты
-	gulp.src('./src/script/*.js')
+	gulp.src("./src/script/*.js")
 		.pipe(jsmin())	
-		.on('error', console.log)
-	    .pipe(gulp.dest('./dist/script'), {overwrite: true}); 
+		.on("error", console.log)
+	    .pipe(gulp.dest("./dist/script"), {overwrite: true}); 
 
 	// перенесем картинки
-	gulp.src('./src/image/*')
-	    .pipe(gulp.dest('./dist/image'), {overwrite: true}); 
+	gulp.src("./src/image/*")
+	    .pipe(gulp.dest("./dist/image"), {overwrite: true}); 
 
-	gulp.src(['./src/view/*/*.js', './src/view/*/*.html'])
-	    .pipe(gulp.dest('./dist/view'), {overwrite: true});    
+	gulp.src(["./src/view/*/*.js", "./src/view/*/*.html"])
+	    .pipe(gulp.dest("./dist/view"), {overwrite: true});    
 	// перенесем index
-	gulp.src('./src/index.html')
-	    .pipe(gulp.dest('./dist'), {overwrite: true});
+	gulp.src("./src/index.html")
+	    .pipe(gulp.dest("./dist"), {overwrite: true});
 
 });
 
